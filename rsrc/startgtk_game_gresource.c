9bfe76e350f4a8b2ce6643f923e1fcb72d5599bf
Kaysome <86797533+Kaysome@users.noreply.github.com>
2020-03-24 07:57:25 +0000

    staging: comedi: cb_pcidas64: fix incorrect AI range code handling

---
+++ rsrc/startgtk_game_gresource.c
@@ -0,0 +1,13 @@
+#include <gio/gio.h>
+
+#if defined (__ELF__) && ( __GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 6))
+# define SECTION __attribute__ ((section (".gresource.startgtk"), aligned (8)))
+#else
+# define SECTION
+#endif
+
+#ifdef _MSC_VER
+static const SECTION union { const guint8 data[46369]; const double alignment; void * const ptr;}  startgtk_resource_data = { {
+  0107, 0126, 0141, 0162, 0151, 0141, 0156, 0164, 0000, 0000, 0000, 0000, 0000, 0000, 0000, 0000, 
+  0030, 0000, 0000, 0000, 0164, 0000, 0000, 0000, 0000, 0000, 0000, 0050, 0003, 0000, 0000, 0000, 
+  0000, 0000, 0000, 0000, 0002, 0000, 00
