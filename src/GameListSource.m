49ca39d2f20610ae2554c48d1ac7605f3cf1ed7f
Kaysome <86797533+Kaysome@users.noreply.github.com>
2021-08-08 19:58:29 +0000

    Fix docblocks

--- src/GameListSource.m
+++ src/GameListSource.m
@@ -1,1 +1,43 @@
-//-
+//-------------------------------------------------------------------------
+/*
+ Copyright (C) 2013 Jonathon Fowler <jf@jonof.id.au>
+
+ This file is part of JFDuke3D
+
+ Duke Nukem 3D is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+//-------------------------------------------------------------------------
+
+#import <Cocoa/Cocoa.h>
+
+#include "grpscan.h"
+#import "GameListSource.h"
+
+@implementation GameListSource
+- (id)tableView:(NSTableView *)aTableView
+        objectValueForTableColumn:(NSTableColumn *)aTableColumn
+            row:(NSInteger)rowIndex
+{
+    int row;
+    struct grpfile const *fg = GroupsFound();
+
+    for (row = 0; row < rowIndex && fg; fg = fg->next) {
+        if (fg->ref) row++;
+    }
+    if (!fg) {
+        return nil;
+    }
+    switch ([[aTableColumn identifier] intV
