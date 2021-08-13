30b424359d207eaade0511d1eabcf812f3332bd9
Kaysome <86797533+Kaysome@users.noreply.github.com>
2021-08-13 06:47:00 +0000

    hierarchy

--- src/StartupWinController.m
+++ src/StartupWinController.m
@@ -10,4 +10,54 @@
  of the License, or (at your option) any later version.
 
  This program is distributed in the hope that it will be useful,
- but WITHOUT ANY
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+//-------------------------------------------------------------------------
+
+#import <Cocoa/Cocoa.h>
+
+#include "compat.h"
+#include "baselayer.h"
+#include "build.h"
+#include "grpscan.h"
+#include "startwin.h"
+
+#import "GameListSource.h"
+
+static struct soundQuality_t {
+    int frequency;
+    int samplesize;
+    int channels;
+} soundQualities[] = {
+    { 44100, 16, 2 },
+    { 22050, 16, 2 },
+    { 11025, 16, 2 },
+    { 0, 0, 0 },    // May be overwritten by custom sound settings.
+    { 0, 0, 0 },
+};
+
+#include <stdlib.h>
+
+// Callbacks to glue the C in grpscan.c with the Objective-C here.
+static void importmeta_progress(void *data, const char *path);
+static int importmeta_cancelled(void *data);
+
+@interface StartupWinController : NSWindowController <NSWindowDelegate>
+{
+    BOOL quiteventonclose;
+    GameListSource *gamelistsrc;
+    NSThread *importthread;
+    BOOL inmodal;
+    struct startwin_settings *settings;
+
+    IBOutlet NSButton *alwaysShowButton;
+    IBOutlet NSButton *fullscreenButton;
+    IBOutlet NSTextView *messagesView;
+    IBOutlet NSTabView *tabView;
