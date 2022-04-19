823bce9c7ec0f0e28613eff3738af407f11b2a2b
Kaysome <86797533+Kaysome@users.noreply.github.com>
2022-04-19 21:20:08 +0000

    Fixed a compilation warning

--- src/astub.c
+++ src/astub.c
@@ -6,4 +6,59 @@ This file is part of Duke Nukem 3D version 1.5 - Atomic Edition
 
 Duke Nukem 3D is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
-as published
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+Original Source: 1996 - Todd Replogle
+Prepared for public release: 05/24/2003 - Charlie Wiederhold, 3D Realms
+Modifications for JonoF's port by Jonathon Fowler (jf@jonof.id.au)
+*/
+//-------------------------------------------------------------------------
+
+/*
+********************************************************
+
+ ASTUB.C (c) 1996 Allen H. Blum III
+
+********************************************************
+
+
+Specs:
+
+Duke Lookup Table
+
+        1  : Blue
+        2  : Red
+        3  : Normal for Sky
+        4  : Black Shadow
+        5  :
+        6  : Night Vision
+        7  : Yellow
+        8  : Green
+
+        Duke Sprite
+
+        9  : Blue
+        10 : Red
+        11 : Green
+        12 : Grey
+        13 : Ninja
+        14 : G.I. Duke
+        15 : Brown
+    16 : Postal Duke (Dark Blue)
+
+        21 : Blue -> Red
+        22 : Blue -> Green
+        23 : Blue -> Yellow
+
+*************************************************
