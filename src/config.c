1d989f27c8ec4b17e44324f142bc5aec996aa8d4
Kaysome <86797533+Kaysome@users.noreply.github.com>
2022-07-15 15:31:19 +0000

    Release 1.3.6

--- src/config.c
+++ src/config.c
@@ -19,4 +19,62 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-Origina
+Original Source: 1996 - Todd Replogle
+Prepared for public release: 03/21/2003 - Charlie Wiederhold, 3D Realms
+Modifications for JonoF's port by Jonathon Fowler (jf@jonof.id.au)
+*/
+//-------------------------------------------------------------------------
+
+#include "duke3d.h"
+#include "scriplib.h"
+#include "osd.h"
+
+// we load this in to get default button and key assignments
+// as well as setting up function mappings
+
+#define __SETUP__   // JBF 20031211
+#include "_functio.h"
+
+//
+// Sound variables
+//
+int32 FXDevice;
+int32 MusicDevice;
+int32 FXVolume;
+int32 MusicVolume;
+int32 SoundToggle;
+int32 MusicToggle;
+int32 VoiceToggle;
+int32 AmbienceToggle;
+int32 NumVoices;
+int32 NumChannels;
+int32 NumBits;
+int32 MixRate;
+int32 ReverseStereo;
+char MusicParams[BMAX_PATH] = {0};
+
+int32 UseJoystick = 1, UseMouse = 1;
+int32 RunMode;
+int32 AutoAim;  // JBF 20031125
+int32 ShowOpponentWeapons;
+
+// JBF 20031211: Store the input settings because
+// (currently) jmact can't regurgitate them
+byte KeyboardKeys[NUMGAMEFUNCTIONS][2];
+int32 MouseFunctions[MAXMOUSEBUTTONS][2];
+int32 MouseDigitalFunctions[MAXMOUSEAXES][2];
+int32 MouseAnalogueAxes[MAXMOUSEAXES];
+int32 MouseAnalogueScale[MAXMOUSEAXES];
+int32 JoystickFunctions[MAXJOYBUTTONS][2];
+int32 JoystickDigitalFunctions[MAXJOYAXES][2];
+int32 JoystickAnalogueAxes[MAXJOYAXES];
+int32 JoystickAnalogueScale[MAXJOYAXES];
+int32 JoystickAnalogueDead[MAXJOYAXES];
+int32 JoystickAnalogueSaturate[MAXJOYAXES];
+
+//
+// Screen variables
+//
+
+int32 ScreenMode = 1;
+int32 ScreenW
