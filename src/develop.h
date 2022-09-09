513aa9409a8cc47e5fcfc43db8aaf55f71b2262b
Kaysome <86797533+Kaysome@users.noreply.github.com>
2022-09-09 16:41:30 +0000

    solar export

--- src/develop.h
+++ src/develop.h
@@ -18,3 +18,49 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+Original Source: 1996 - Todd Replogle
+Prepared for public release: 03/21/2003 - Charlie Wiederhold, 3D Realms
+Modifications for JonoF's port by Jonathon Fowler (jf@jonof.id.au)
+*/
+//-------------------------------------------------------------------------
+
+#ifndef _develop_public
+#define _develop_public
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DEVELOPMENT          0
+#define SHAREWARE            0
+#define LOCATIONINFO         1
+#define SOFTWAREERROR        1
+#define MEMORYCORRUPTIONTEST 1
+#define PRECACHETEST         0
+#define DATACORRUPTIONTEST   0
+#define RANDOMNUMBERTEST     0
+
+
+#if ( LOCATIONINFO == 1 )
+
+#define funcstart() \
+   { \
+   SoftError( "funcstart : module '%s' at line %d.\n", __FILE__, __LINE__ );\
+   }
+
+#define funcend() \
+   { \
+   SoftError( "  funcend : module '%s' at line %d.\n", __FILE__, __LINE__ );\
+   }
+
+#else
+
+#define funcstart()
+#define funcend()
+
+#endif
+
+#ifdef __cplusplus
+};
+#endif
+#endif
